54e15805f0a0a0d6006da3fe8e8c0c5e9e8cda58
Flamethrower <ignaciovallejoalcalde@gmail.com>
2025-09-29 23:18:55 +0200

    Update ArduinoDS4.cpp

--- src/ArduinoDS4.cpp
+++ src/ArduinoDS4.cpp
@@ -1,1 +1,12 @@
+#include "ArduinoDS4.h"
+
+#if defined(USBCON)
+	#ifndef ARDUINODS4_USB
+		#warning "You need to select [____ as DS4] in the boards tab to be able to communicate with your PlayStation!"
+	#endif
+#else
+	#ifndef USB_XINPUT
+		#error "Warning: this board does not support HID. It will not work with ArduinoDS4."
+	#endif
+#endif
 
