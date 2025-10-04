020d44b2294a63aba2cda03b6a3d521f806aa7be
Flamethrower <ignaciovallejoalcalde@gmail.com>
2025-10-05 00:50:06 +0200

    Update ArduinoPS4.cpp

--- src/ArduinoPS4.cpp
+++ src/ArduinoPS4.cpp
@@ -1,29 +1,30 @@
 #include "ArduinoPS4.h"
 
-#if defined(USBCON) && !defined ARDUINOPS4_USB
+#if defined(USBCON) && !defined(ARDUINOPS4_USB)
 	#warning "You need to select [____ as PS4 controller] in the boards tab to be able to communicate with your PlayStation!"
 #endif
 
-void writeToEndpoint(const uint8_t* data, uint8_t len) {
-  for (uint8_t i = 0; i < len; i++) {
-    UEDATX = data[i];
-  }
+void ArduinoPS4::writeToEndpoint(const uint8_t* data, uint8_t len) {
+  	for (uint8_t i = 0; i < len; i++) {
+    	UEDATX = data[i];
+  	}
 }
 
-void txInterruptCallback() {
-  reportCounter = (reportCounter + 1) & 0x3F;
-  timestamp++;
-  report.axisTiming = timestamp;
-  writeToEndpoint((const uint8_t*)&report, sizeof(report));
-  UEINTX &= ~(1 << FIFOCON);
+void ArduinoPS4::txInterruptCallback() {
+  	arduinoPS4.reportCounter = (arduinoPS4.reportCounter + 1) & 0x3F;
+	arduinoPS4.report.reportCounter = arduinoPS4.reportCounter;
+  	arduinoPS4.timestamp++;
+  	arduinoPS4.report.axisTiming = arduinoPS4.timestamp;
+  	arduinoPS4.writeToEndpoint((const uint8_t*)&arduinoPS4.report, sizeof(arduinoPS4.report));
+  	UEINTX &= ~(1 << FIFOCON);
 }
 
-void controllerOutHandler() {
+void ArduinoPS4::controllerOutHandler() {
 	uint8_t buf[64];
     int n = USB_Recv(ARDUINOPS4_RX_ENDPOINT, buf, USB_Available(ARDUINOPS4_RX_ENDPOINT));
 }
 
-void arduinoPS4.begin(){
+void ArduinoPS4::begin(){
 	memset(&report, 0, sizeof(report));
 	report.reportID = 0x01;
 	report.leftStickX = 0x80;
@@ -35,14 +36,14 @@ void arduinoPS4.begin(){
 	report.buttonSouth = 0x00;
 	report.buttonEast = 0x00;
 	report.buttonWest = 0x00;
-	report.buttonL1 = 
-	report.buttonL2
-	report.buttonL3
-	report.buttonR1
-	report.buttonR2
-	report.buttonR3
+	report.buttonL1 = 0x00;
+	report.buttonL2 = 0x00;
+	report.buttonL3 = 0x00;
+	report.buttonR1 = 0x00;
+	report.buttonR2 = 0x00;
+	report.buttonR3 = 0x00;
 	report.buttonHome = 0x00;
-	report.buttonTouchpad
+	report.buttonTouchpad = 0x00;
 	report.reportCounter = reportCounter;
 	report.leftTrigger = 0x00;
 	report.rightTrigger = 0x00;
@@ -67,7 +68,16 @@ void arduinoPS4.begin(){
   	sei();
 }
 
-void setDpad(dirEnum direction) {
+void ArduinoPS4::maintainConnection() {
+  	if (millis() - lastReconnect >= 480000) {
+    	UDCON |= (1 << DETACH);
+  		delay(100);
+  		UDCON &= ~(1 << DETACH);
+    	lastReconnect = millis();
+  	}
+}
+
+void ArduinoPS4::setDpad(dirEnum direction) {
 	if(direction == 1) {report.dpad = 0;}
 	else if(direction == 2) {report.dpad = 1;}
 	else if(direction == 3) {report.dpad = 2;}
@@ -79,7 +89,7 @@ void setDpad(dirEnum direction) {
 	else if(direction == 9) {report.dpad = 8;}
 }
 
-void setButton(buttonEnum button, bool state) {
+void ArduinoPS4::setButton(buttonEnum button, bool state) {
 	if(button == 1) {report.buttonNorth = state;}
 	else if(button == 2) {report.buttonEast = state;}
 	else if(button == 3) {report.buttonSouth = state;}
@@ -96,35 +106,14 @@ void setButton(buttonEnum button, bool state) {
 	else if(button == 14) {report.buttonTouchpad = state; }
 }
 
-void setTrigger(sideEnum trigger, uint8_t value) {
-	if(trigger == 1) {report.leftTrigger == value;}
-	else if(trigger == 2) {report.rightTrigger == value;}
+void ArduinoPS4::setTrigger(sideEnum trigger, uint8_t value) {
+	if(trigger == 1) {report.leftTrigger = value;}
+	else if(trigger == 2) {report.rightTrigger = value;}
 }
 
-void setJoystick(sideEnum joystick, axisEnum axis, uint8_t value) {
+void ArduinoPS4::setJoystick(sideEnum joystick, axisEnum axis, uint8_t value) {
 	if(joystick == 1 && axis == 1) {report.leftStickX = value;}
 	else if(joystick == 1 && axis == 2) {report.leftStickY = value;}
 	else if(joystick == 2 && axis == 1) {report.rightStickX = value;}
 	else if(joystick == 2 && axis == 2) {report.rightStickY = value;}
-}
-
-void loop(){
-	if(ArduinoPS4Active == true) {
-		unsigned long currentTime = millis();
-		if (currentTime - lastReport >= 5) {
-    		lastReport = currentTime;
-    		reportCounter = (reportCounter + 1) & 0x3F;
-    		report.reportCounter = reportCounter;
-    		timestamp++;
-    		report.axisTiming = timestamp;
-			
-    		noInterrupts();
-    		UENUM = ARDUINOPS4_TX_ENDPOINT;
-		    if (UEINTX & (1 << RWAL)) {
-		      writeToEndpoint((const uint8_t*)&report, sizeof(report));
-		      UEINTX &= ~(1 << FIFOCON);
-		    }
-    		interrupts();
-		}
-	}
 }
